3e0dea4c73aa8f5fad3d92ba0ad32110349f86d4
Baptiste Drillien <baptistedrillien@MacBook-Pro-de-Baptiste.local>
2023-05-09 15:16:16 +0200

    added label in search input

--- src/modules/search-movies/application/components/search-input.tsx
+++ src/modules/search-movies/application/components/search-input.tsx
@@ -8,16 +8,22 @@ interface Props {
 export const SearchInput = ({ query, handleOnQueryChange }: Props) => {
 	return (
 		<div className="flex gap-6 items-center mb-8">
-			<Icon
-				name="search"
-				className="w-6 h-6 text-gray-500 dark:text-gray-400"
-			/>
+			<label
+				htmlFor="search-input"
+				className="flex items-center justify-center"
+			>
+				<Icon
+					name="search"
+					className="w-6 h-6 text-gray-500 dark:text-gray-400"
+				/>
+			</label>
 			<input
 				value={query}
 				onChange={handleOnQueryChange}
 				className="h-11 flex-auto rounded-md px-4 bg-gray-800 border border-gray-700 text-white"
 				type="text"
 				placeholder="Rechercher un titre de film..."
+				id="search-input"
 			/>
 		</div>
 	)
