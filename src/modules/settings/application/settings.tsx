54ed19e8822fb3d78076b936de7612f8cf724efa
Baptiste Drillien <baptistedrillien@MacBook-Pro-de-Baptiste.local>
2023-05-16 18:07:48 +0200

    login and register pages redesign

--- src/modules/settings/application/settings.tsx
+++ src/modules/settings/application/settings.tsx
@@ -30,7 +30,7 @@ export const Settings = () => {
 			<button
 				onClick={() => onSignOut()}
 				type="button"
-				className="flex items-center text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+				className="flex items-center text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
 			>
 				<Icon className="mr-2" name="logout" />
 				Se déconnecter
