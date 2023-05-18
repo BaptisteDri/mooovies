2721f22617785e7adaaea0af3a31444b751bc03d
Baptiste Drillien <baptistedrillien@MacBook-Pro-de-Baptiste.local>
2023-05-18 12:02:50 +0200

    fixed share arrow function

--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -5,15 +5,13 @@ interface Props {
 }
 
 export const ShareButton = ({ url }: Props) => {
-	const _onShare = async () => {}
-
 	return (
 		<button
-			onClick={() => _onShare()}
 			type="button"
 			className="flex items-center text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-3 py-2 text-center"
 		>
-			<Icon name="ios_share" className="mr-2 text-base" /> Partager
+			<Icon name="ios_share" className="sm:mr-2 sm:leading-4 text-base" />
+			<span className="hidden sm:block">Partager</span>
 		</button>
 	)
 }
