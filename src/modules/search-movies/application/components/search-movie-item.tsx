bf0619b7a5f92d5991469fac681cfea745c05a52
Baptiste Drillien <baptiste.drillien@live.fr>
2023-07-29 13:59:09 +0200

    feat: better responsive button

--- src/modules/search-movies/application/components/search-movie-item.tsx
+++ src/modules/search-movies/application/components/search-movie-item.tsx
@@ -55,7 +55,7 @@ export const SearchMovieItem = ({
 					onClick={() => setSelectedMovie(movie)}
 					className="whitespace-nowrap font-medium rounded-lg text-sm px-3 py-2 ml-3 sm:ml-4 h-fit text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 text-center"
 				>
-					Ajouter à ma liste
+					Ajouter<span className="hidden sm:flex"> à ma liste</span>
 				</button>
 			)}
 		</li>
