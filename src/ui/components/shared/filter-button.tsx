2adbe5128d4b585785915db3069597d09f021ddf
Baptiste Drillien <baptiste.drillien@live.fr>
2024-02-26 18:06:39 +0100

    feat: improved filter buttons style

--- src/ui/components/shared/filter-button.tsx
+++ src/ui/components/shared/filter-button.tsx
@@ -12,7 +12,7 @@ export const FilterButton = ({ isActive, onClick, title }: Props) => {
 	return (
 		<button
 			className={mCn(
-				"flex items-center justify-center h-8 px-2  border rounded-xl cursor-pointer hover:text-slate-300 border-slate-700 text-slate-400 bg-slate-800 hover:bg-slate-700",
+				"flex items-center justify-center h-8 px-2.5  border rounded-xl cursor-pointer hover:text-slate-300 border-slate-700 text-slate-400 bg-slate-800 hover:bg-slate-700 text-sm",
 				isActive && "text-slate-300 border-blue-600"
 			)}
 			onClick={onClick}
